8dd22bcc78dc8cab92b3462985bde4d57f607598
anytizer <anytizer@gmail.com>
2020-04-12 22:52:51 -0600

    add form

--- ts/add.ts
+++ ts/add.ts
@@ -7,14 +7,16 @@
         <a href="/{table}/list" class="w3-green w3-btn">Back</a>
     </div>
 
-    <form name='add' action='/{table}/add/' method='post' onsubmit="document.getElementById('{table}-submit').disabled=true; return true;">
-        {htmls_add}
+    <form name='add' action='/{table}/add/' method='post' onsubmit="document.getElementById('{table}-submit').disabled=true; return true;" autocomplete="off">
+
+        {__htmls_add__}
+        
+        {__hidden__}
+
         <div class="w3-row w3-padding">
             <div class='w3-col l2'>&nbsp;</div>
             <div class='w3-col l10'>
                 <input type="hidden" name="{pk_id}" value="{{ guid }}" />
-                <!-- other hidden columns -->
-                {hidden}
                 <input type="submit" id="{table}-submit" name="" value="Add" class="w3-green w3-btn" />
                 <a href="/{table}">Cancel</a>
             </div>
