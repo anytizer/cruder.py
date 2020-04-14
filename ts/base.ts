aeb99b0f68e1a48ddddc66500c7b506369bfae0b
anytizer <anytizer@gmail.com>
2020-04-13 23:49:40 -0600

    AngularJS - old copy linked

--- ts/base.ts
+++ ts/base.ts
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="crud">
 <head>
     <title>{% block title %}{% endblock %}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
     <link rel="stylesheet" href="/static/style.css" />
 
+    <!--
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>$(function(){$("#datepicker").datepicker({dateFormat:"yy-mm-dd"})});</script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.9/angular.min.js"></script>
+    <script src="/static/controller.js"></script>
+    -->
 </head>
 <body class='w3-sand'>
 
