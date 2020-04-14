f10a8ea30b22d8c0227a27d1a61ba6b96807dbca
anytizer <anytizer@gmail.com>
2020-04-13 23:28:32 -0600

    extra_options on top

--- ts/details.ts
+++ ts/details.ts
@@ -4,16 +4,15 @@
 {% block content %}
     <div class='w3-container w3-pale-yellow w3-padding'>
         <a href="/{table}/list" class="w3-green w3-btn">Back</a>
+        {extra_options}
     </div>
 
     {detail_fields}
 
-    <!-- @todo Extra Flags -->
-
     <div class="w3-container w3-pale-red w3-padding">
         <div class="w3-left">
             <a class='w3-btn w3-red' href="/{table}/edit/{{data.{pk_id}}}">Edit</a>
-            {detail_extras}
+
         </div>
         <div class="w3-right">
             Are you sure? <a class='w3-btn w3-red' href="/{table}/delete/{{data.{pk_id}}}">Delete</a>
