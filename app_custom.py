19f4857289566cb23ce4c77b5dddc186545e9d98
anytizer <anytizer@gmail.com>
2020-04-14 14:15:44 -0600

    use database, not exporter

--- app_custom.py
+++ app_custom.py
@@ -1,9 +1,10 @@
+import re
+
 from flask import Flask, Response, render_template, request, redirect
 from flask import Blueprint
 
 import meta
 from database import database
-from database import exporter
 
 
 bp = Blueprint("BP_CUSTOM_ENDPOINTS", __name__, template_folder="templates", static_folder="static")
@@ -12,17 +13,20 @@ bp = Blueprint("BP_CUSTOM_ENDPOINTS", __name__, template_folder="templates", sta
 # from: configurator
 @bp.route("/config_showfields/flag/", methods=["POST"])
 def config_showfields_flag():
-    field =  request.form["field"]
+    field = re.sub(r'[^a-z0-9_]', '', request.form["field"]) # SQL Safety
+    assert field == request.form["field"]
+    assert field == request.form["id"][37:]
+    # @see custom modified list.html
     config_id = request.form["id"][0:36] # ID.FIELD_NAME
     query = f"UPDATE config_showfields set `{field}` = CASE `{field}` WHEN '1' THEN '0' ELSE '1' END WHERE config_id=?;"
-    e = exporter()
-    e.query(query, (config_id,))
+    db = database()
+    db.query(query, (config_id,))
     return "Success?"
 
 
 # from: gardens
 @bp.route("/trays/showhide/<tray_id>/")
 def trays_showhide(tray_id):
-    e = exporter()
-    e.query("UPDATE trays SET is_visible=CASE is_visible WHEN '1' THEN '0' ELSE '1' END WHERE tray_id=?;", (tray_id,))
+    db = database()
+    db.query("UPDATE trays SET is_visible=CASE is_visible WHEN '1' THEN '0' ELSE '1' END WHERE tray_id=?;", (tray_id,))
     return redirect("/trays/list/")
